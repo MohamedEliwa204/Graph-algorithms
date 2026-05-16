d88a360a843fb3bdbd070d6bff04e138d9aba886
MohamedElshewy <es.mohamed.eliwa2023@alexu.edu.eg>
2026-05-16 12:56:29 +0300

    Add DataGeneration class for generating various graph types

---
+++ src/main/java/data/intensive/apps/inputDistribution/DataGeneration.java
@@ -0,0 +1,97 @@
+package data.intensive.apps.inputDistribution;
+
+import data.intensive.apps.ds.Edge;
+import data.intensive.apps.ds.Graph;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGeneration {
+
+    public static Graph sparseGraph(){
+        Random random = new Random(42);
+        Graph graph = new Graph();
+        for (int i = 0; i < 5000; i++) {
+            int vertex = i;
+            int numberOfEdges = random.nextInt(10) + 1;
+            boolean[] visited = new boolean[5000];
+            visited[vertex] = true;
+            for (int j = 0; j < numberOfEdges; j++) {
+                int distenation;
+                do {
+                    distenation = random.nextInt(5000);
+                } while (visited[distenation]); // to prevent self loops and parallel edges
+                visited[distenation] = true;
+                int weight = random.nextInt(1000) + 1;
+                graph.addEdge(vertex, distenation, weight);
+
+            }
+        }
+        return graph;
+    }
+
+    public static Graph denseGraph(){
+        Random random = new Random(42);
+        Graph graph = new Graph();
+        for (int i = 0; i < 5000; i++) {
+            int vertex = i;
+            int numberOfEdges = 1250;
+            boolean[] visited = new boolean[5000];
+            visited[vertex] = true;
+            for (int j = 0; j < numberOfEdges; j++) {
+                int distenation;
+                do {
+                    distenation = random.nextInt(5000);
+                } while (visited[distenation]); // to prevent self loops and parallel edges
+                visited[distenation] = true;
+                int weight = random.nextInt(1000) + 1;
+                graph.addEdge(vertex, distenation, weight);
+            }
+        }
+        return graph;
+    }
+
+    public static Graph completedGraph(){
+        Random random = new Random(42);
+        Graph graph = new Graph();
+        for (int i = 0; i < 5000; i++) {
+            int vertex = i;
+            int numberOfEdges = 4999;
+            boolean[] visited = new boolean[4999];
+            visited[vertex] = true;
+            for (int j = 0; j < numberOfEdges; j++) {
+                int distenation;
+                do {
+                    distenation = random.nextInt(5000);
+                } while (visited[distenation]); // to prevent self loops and parallel edges
+                visited[distenation] = true;
+                int weight = random.nextInt(1000) + 1;
+                graph.addEdge(vertex, distenation, weight);
+            }
+        }
+        return graph;
+    }
+
+    public static Graph directedAcyclicGraph(){
+        Random random = new Random(42);
+        Graph graph = new Graph();
+        for (int i = 0; i < 4999; i++) { // last vertex has no edges out of it in DAG
+
+            int vertex = i;
+            int numberOfEdges = random.nextInt(10) + 1;
+            boolean[] visited = new boolean[5000];
+            visited[vertex] = true;
+            for (int j = 0; j < numberOfEdges; j++) {
+                int distenation;
+                do {
+                    distenation = random.nextInt(5000 - i) + i; // to prevent cycles, make edges forward.
+                } while (visited[distenation]); // to prevent self loops and parallel edges
+                visited[distenation] = true;
+                int weight = random.nextInt(1000) + 1;
+                graph.addDirectedEdge(vertex, distenation, weight);
+            }
+        }
+        return graph;
+    }
+
+}
